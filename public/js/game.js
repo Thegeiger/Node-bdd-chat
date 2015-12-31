3c58be6c60ff26cf023ae500757adc1fd313c55d
adfinitas42 <adfinitas42@dhcp118.adfinitas.fr>
2015-12-31 14:58:27 +0100

    push start game.js hero

---
+++ public/js/game.js
@@ -0,0 +1,20 @@
+function {
+	// une fonction qui recupaire a chaque tour des info divisé en hero, adv, action, map
+	// Initialisation d'un hero par default et création de l'objet qui sera utilisé sur le serv
+
+	// structure personnage principale servira a faire des classe hérité pour les mosntre et hero
+	var hero = {
+		mp: 3,
+		ap: 10,
+		hp: 60,
+		// caract ?
+		stuff: {
+			
+		}
+		// tmp calculer a partire des cac + arme
+		dmg_c: 10,
+		dmg_m: 7,
+		dmg_d: 8,
+		armor: 10,
+	}
+}
