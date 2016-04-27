871911a2ba615d87e3a6956df76dab7b9c3a6405
geiger_a <anthony.geiger@epitech.eu>
2016-04-27 22:01:28 +0000

    ground/hero

---
+++ Person.js
@@ -0,0 +1,52 @@
+var basic_class = {
+  MAGE : {
+  	value: 0, MP: 3, HP: 300
+  },
+  SUMONERS {
+  	value: 1, MP: 3, HP: 350
+  },
+  ARCHER {
+  	value: 2, MP: 4, HP: 400
+  },
+  PALADIN : {
+  	value: 3, MP: 3, HP: 600
+  },
+  BARBAR : {
+  	value: 4, MP: 4, HP: 500
+  }
+  ROGUE : {
+  	value: 5, MP: 5, HP: 400
+  }
+  SCOOT : {
+    value: 6, MP: 4, HP: 400
+  }
+  BARDE : {
+    value: 7, MP 4, HP: 400
+  }
+};
+
+var hero = {
+	name: "",
+	_class: null,
+	lvl: 0,
+	carac: {
+		force: 1,
+		intel: 1,
+		agi: 1,
+		sagesse: 1,
+    charisme: 1,
+    constitution: 1
+	},
+  stat: {
+    HP: 0,
+    MP: 0,
+    AP: 10,
+    will: 0,
+    reflex: 0,
+    vigor: 0
+  },
+  competence: {
+    detection: 0,
+    
+  }
+}
